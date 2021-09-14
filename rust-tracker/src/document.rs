b88c76b3247c4134f2d03940696bed7177d31cc4
Simon Kågedal Reimer <skagedal@gmail.com>
2021-09-14 09:04:11 +0200

    Some work on parser

--- rust-tracker/src/document.rs
+++ rust-tracker/src/document.rs
@@ -1,6 +1,6 @@
 use chrono::{NaiveDate, NaiveTime};
-use regex::Regex;
-use crate::document::Line::Comment;
+use regex::{Regex, Captures, CaptureLocations};
+use crate::document::Line::{Comment, DayHeader, OpenShift};
 
 #[derive(PartialEq, Debug)]
 pub enum Line {
@@ -48,6 +48,14 @@ pub struct Parser {
     blank_regex: Regex,
 }
 
+fn get_i32(m: &Captures, name: &str) -> i32 {
+    m.name(name).unwrap().as_str().parse::<i32>().unwrap()
+}
+
+fn get_u32(m: &Captures, name: &str) -> u32 {
+    m.name(name).unwrap().as_str().parse::<u32>().unwrap()
+}
+
 impl Parser {
     fn new() -> Self {
         Parser {
@@ -63,8 +71,10 @@ impl Parser {
 
     fn parse_line(self: &Self, string: &str) -> Option<Line> {
         self.parse_comment(string)
+            .or_else(|| self.parse_day_header(string))
+            .or_else(|| self.parse_open_shift(string))
             // TODO: Here, other parsers
-            .or_else(|| Option::None)
+            .or_else(|| None)
     }
 
     fn parse_comment(self: &Self, string: &str) -> Option<Line> {
@@ -72,13 +82,34 @@ impl Parser {
             text: String::from(m.name("text").unwrap().as_str())
         })
     }
+
+    fn parse_day_header(self: &Self, string: &str) -> Option<Line> {
+        self.day_header_regex.captures(string).map(|m| DayHeader {
+            date: NaiveDate::from_ymd(
+                get_i32(&m, "year"),
+                get_u32(&m, "month"),
+                get_u32(&m, "day")
+            )
+        })
+    }
+
+    fn parse_open_shift(self: &Self, string: &str) -> Option<Line> {
+        self.open_shift_regex.captures(string).map(|m| OpenShift {
+            start_time: NaiveTime::from_hms(
+                get_u32(&m, "hour"),
+                get_u32(&m, "minute"),
+                0
+            )
+        })
+    }
 }
 
 #[cfg(test)]
 mod tests {
     use regex::Regex;
-    use crate::document::Parser;
-    use crate::document::Line::Comment;
+    use crate::document::{Parser, Document, Day};
+    use crate::document::Line::{Comment, SpecialDay, Blank, ClosedShift, SpecialShift, OpenShift, DayHeader};
+    use chrono::{NaiveDate, NaiveTime};
 
     #[test]
     fn read_line() {
@@ -87,6 +118,128 @@ mod tests {
         assert_eq!(
             Option::Some(Comment { text: String::from("hello") }),
             parser.parse_line("# hello")
-        )
+        );
+
+        assert_eq!(
+            Option::Some(DayHeader { date: NaiveDate::from_ymd(2021, 9, 13)}),
+            parser.parse_line("[monday 2021-09-13]")
+        );
+
+        assert_eq!(
+            Option::Some(OpenShift { start_time: time_hm(8, 12) }),
+            parser.parse_line("* 08:12-")
+        );
+
+        assert_eq!(
+            Option::Some(ClosedShift { start_time: time_hm(8, 24), stop_time: time_hm(9, 12)}),
+            parser.parse_line("* 08:24-09:12")
+        );
+
+        assert_eq!(
+            Option::Some(SpecialDay { text: String::from("hello")}),
+            parser.parse_line("* hello")
+        );
+
+        assert_eq!(
+            Option::Some(SpecialShift { text: String::from("VAB"), start_time: time_hm(13, 5), stop_time: time_hm(20, 2)}),
+            parser.parse_line("* VAB 13:05-20:02")
+        );
+
+        assert_eq!(
+            Option::Some(Blank),
+            parser.parse_line("")
+        );
+
+    }
+
+    #[test]
+    fn serialize_deserialize() {
+        let serializedForm = "[monday 2020-07-13]
+* Vacation
+# Came back from Jämtland
+
+[tuesday 2020-07-14]
+* 08:32-12:02
+* 12:30-13:01
+* 13:45-18:03
+
+[wednesday 2020-07-15]
+* 11:00-18:00
+
+[thursday 2020-07-16]
+* 08:00-12:00
+* VAB 13:00-17:00
+
+[friday 2020-07-17]
+* 08:12-
+";
+        let document = Document {
+            preamble: vec![],
+            days: vec![
+                Day {
+                    date: NaiveDate::from_ymd(2020, 7, 13),
+                    lines: vec![
+                        SpecialDay { text: String::from("Vacation") },
+                        Comment { text: String::from("Came back from Jämtland")},
+                        Blank
+                    ]
+                },
+                Day {
+                    date: NaiveDate::from_ymd(2020, 7, 14),
+                    lines: vec![
+                        ClosedShift {
+                            start_time: time_hm(8, 32),
+                            stop_time: time_hm(12, 2)
+                        },
+                        ClosedShift {
+                            start_time: time_hm(12, 30),
+                            stop_time: time_hm(13, 1)
+                        },
+                        ClosedShift {
+                            start_time: time_hm(13, 45),
+                            stop_time: time_hm(18, 3)
+                        },
+                        Blank
+                    ]
+                },
+                Day {
+                    date: NaiveDate::from_ymd(2020, 7, 15),
+                    lines: vec![
+                        ClosedShift {
+                            start_time: time_hm(11, 0),
+                            stop_time: time_hm(18, 0)
+                        },
+                        Blank
+                    ]
+                },
+                Day {
+                    date: NaiveDate::from_ymd(2020, 7, 16),
+                    lines: vec![
+                        ClosedShift {
+                            start_time: time_hm(8, 0),
+                            stop_time: time_hm(12, 0)
+                        },
+                        SpecialShift {
+                            text: String::from("VAB"),
+                            start_time: time_hm(13, 0),
+                            stop_time: time_hm(17, 0)
+                        },
+                        Blank
+                    ]
+                },
+                Day {
+                    date: NaiveDate::from_ymd(2020, 7, 17),
+                    lines: vec![
+                        OpenShift {
+                            start_time: time_hm(8, 12)
+                        }
+                    ]
+                }
+            ]
+        };
+    }
+
+    fn time_hm(hour: u32, minute: u32) -> NaiveTime {
+        NaiveTime::from_hms(hour, minute, 0)
     }
 }
